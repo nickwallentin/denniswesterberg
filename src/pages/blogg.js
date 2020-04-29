2ee2015e68e93a3a46ce9ce07a071d33fca7b37d
Niklas Wallentin <niklaswallentin@Niklass-MacBook-Pro.local>
2020-04-30 00:09:01 +0200

    last commit for today

--- src/pages/blogg.js
+++ src/pages/blogg.js
@@ -7,7 +7,15 @@ import SEO from "../components/seo"
 
 import NotificationsIcon from "../assets/svg/notifications.svg"
 
-import { Container, Box, Flex, Grid, Heading, Button } from "../styles/elements"
+import {
+  Container,
+  Box,
+  Flex,
+  Grid,
+  Heading,
+  Button,
+  Text,
+} from "../styles/elements"
 
 const BlogPage = () => {
   return (
@@ -15,11 +23,16 @@ const BlogPage = () => {
       <SEO title="Blogg"></SEO>
       <Box bg="var(--c-bg-sec)">
         <Container>
-          <Grid gridTemplateColumns={["1fr 1fr", "2fr 1fr"]} py="5">
+          <Grid gridTemplateColumns={["1fr", "2fr 1fr"]} py="5">
             <Box>
-              <Heading fontSize="3.5rem" as="h1">
+              <Heading mb="4" fontSize="3.5rem" as="h1">
                 Blogg
               </Heading>
+              <Text>
+                Det finns en djupt rotad föreställning hos oss människor, om att
+                stress är en naturlig och ofrånkomlig del av livet. Dessutom en
+                viktig ingrediens för framgång.
+              </Text>
             </Box>
             <Flex alignItems="center" justifyContent="flex-end">
               <Button py={["2", "3"]} px={["2", "4"]} bg="var(--c-bg-pop)">
